74e898b918ab3dcda5b6580e35b8538c770ff473
Yasir Mumtaz <104060049+Yasirmumta@users.noreply.github.com>
2022-07-23 10:03:18 +0530

    Add files via upload

---
+++ data.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int amount1, amount2, total=0;
+    cin>>amount1;
+    cin>>amount2;
+    total = amount1 + amount2;
+    cout<<total;
+    return 0;
+}
